b94dc6d0c2ff757d3e2435f36ce1330915e547a9
Scott Kim <Scott.T.Kim@gmail.com>
2017-01-15 07:57:10 -0500

    Added Greeting Controller Unit Test.

---
+++ src/test/java/com/apis/management/controller/GreetingControllerTest.java
@@ -0,0 +1,16 @@
+package com.apis.management.controller;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by scottkim on 1/15/17.
+ */
+public class GreetingControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
