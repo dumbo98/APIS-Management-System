e26f4539b29613d1c614415b13d9d4a2e8fc01f2
Scott Kim <Scott.T.Kim@gmail.com>
2017-01-15 00:08:28 -0500

    Initial version.

---
+++ src/main/java/com/apis/management/ApisManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.apis.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisManagementSystemApplication.class, args);
+	}
+}
